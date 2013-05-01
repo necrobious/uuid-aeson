c689d586ab8710e5e23b90d776a6f3578b8158e8
Kirk Peterson <necrobious@gmail.com>
2013-05-01 13:22:35 -0700

    initial

---
+++ src/Data/UUID/Aeson.hs
@@ -0,0 +1,21 @@
+module Data.UUID.Aeson where
+
+import Control.Applicative ((<$>), (<*>), pure)
+
+import Data.UUID (UUID, fromString)
+
+import Data.Aeson
+import Data.Aeson.Types
+
+import qualified Data.Text as T
+
+instance ToJSON UUID where
+  toJSON = String . T.pack . show
+
+instance FromJSON UUID where
+  parseJSON json@(String t) = 
+    let uuidString = T.unpack t
+    in case fromString uuidString of
+         Just uuid -> pure uuid 
+         Nothing   -> typeMismatch "UUID" json 
+  parseJSON unknown = typeMismatch "UUID" unknown
